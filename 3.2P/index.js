32ab70aa235f32e3ba69e804ef15549f0acb56e1
Jordan Anderson <andersonjor@deakin.edu.au>
2026-07-14 15:34:29 +1000

    add books page with cards for each book

--- 3.2P/index.js
+++ 3.2P/index.js
@@ -3,6 +3,15 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(port,()=>{
-    console.log("App listening to: "+port)
-})
+app.use(express.static(__dirname + "/public"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/books", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "books.html"));
+});
+
+app.listen(port, () => {
+  console.log("App listening to: " + port);
+});
